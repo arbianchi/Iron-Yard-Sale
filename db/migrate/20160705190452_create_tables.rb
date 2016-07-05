7b0bd0a46dc642b6a79d98be52e60010db26356b
Zach H <zdhadgraft@gtcc.edu>
2016-07-05 16:54:52 -0400

    Added migration.

---
+++ db/migrate/20160705190452_create_tables.rb
@@ -0,0 +1,48 @@
+class CreateTables < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.integer     :name,              null: false
+      t.text        :description,       null: false
+      t.integer     :price_in_cents,    null: false
+      t.integer     :store_id           null: false
+
+      t.timestamps
+    end
+
+    create_table :inventory do |t|
+      t.integer     :store_id,      null: false
+      t.integer     :item_id,       null: false
+      t.integer     :quantity,      null: false
+
+      t.timestamps
+    end
+
+    create_table :transactions do |t|
+      t.integer     :store_id,  null: false
+      t.integer     :item_id,   null: false
+      t.integer     :price_in_cents,     null: false
+      t.integer     :quantity_purchased null: false
+      t.integer     :buyer_id,  null: false
+
+      t.timestamps
+    end
+
+    create_table :store_admins do |t|
+      t.integer     :store_id,  null: false
+      t.integer     :user_id,   null: false
+    end
+
+    create_table :watched_items do |t|
+      t.integer     :item_id,   null: false
+      t.integer     :user_id,   null: false
+      t.integer     :price_in_cents, null: false
+    end
+
+    create_table :stores do |t|
+      t.string      :name,          null: false
+      t.text        :description,   null: false
+    end
+
+    add_column :users, :admin,  :boolean
+
+end
