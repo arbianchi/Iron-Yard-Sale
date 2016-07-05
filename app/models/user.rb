2cb7e2969be7e3f0430aaa499f0774679bfbdb53
Zach H <zdhadgraft@gtcc.edu>
2016-07-05 19:44:16 -0400

    Added associations to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :store
+  has_one :store, :through          => :store_admins
+
+  has_many :transactions
+
+  has_many :watched_items
+  has_many :items, :through         => :watched_items
 end
