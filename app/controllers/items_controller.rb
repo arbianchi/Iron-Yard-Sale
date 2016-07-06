fdff2e472a4632d7a666c8677dc1c663a140bd9a
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-07-06 08:31:21 -0400

    started item and store controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,65 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+    # authorize @items
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to @item, notice: 'item was successfully created.' }
+        format.json { render :show, status: :created, location: @item }
+      else
+        format.html { render :new }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @item.update(item_params)
+        format.html { redirect_to @item, notice: 'item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @item }
+      else
+        format.html { render :edit }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to items_url, notice: 'item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def item_params
+    params.require(:item).permit(:store_id, :price_in_cents, :description)
+  end
 end
--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,2 +1,69 @@
 class StoresController < ApplicationController
+
+  def index
+    @stores = Store.all
+    # authorize @stores
+  end
+
+  def show
+    @store = Store.find(params[:id])
+  end
+
+  def new
+    @store = Store.new
+  end
+
+  def edit
+    @store = Store.find(params[:id])
+  end
+
+  def create
+    @store = Store.new(store_params)
+
+    respond_to do |format|
+      if @store.save
+        format.html { redirect_to @store, notice: 'Store was successfully created.' }
+        format.json { render :show, status: :created, location: @store }
+      else
+        format.html { render :new }
+        format.json { render json: @store.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @store.update(store_params)
+        format.html { redirect_to @store, notice: 'store was successfully updated.' }
+        format.json { render :show, status: :ok, location: @store }
+      else
+        format.html { render :edit }
+        format.json { render json: @store.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @store.destroy
+    respond_to do |format|
+      format.html { redirect_to stores_url, notice: 'store was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def watch_item
+
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_store
+    @store = store.find(params[:id])
+  end
+
+  def store_params
+    params.require(:store).permit(:name, :description)
+  end
+
 end
