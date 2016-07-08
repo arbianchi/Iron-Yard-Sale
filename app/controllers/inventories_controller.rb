fb09a2bd9ffbe0ff0d42586e2dd49435688e1f47
Zach H <zdhadgraft@gtcc.edu>
2016-07-08 09:50:55 -0400

    WIP improving CSV importing.

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -3,16 +3,22 @@ class InventoriesController < ApplicationController
   def upload
     hashes = SmarterCSV.process(params[:file].tempfile)
     hashes.each do |record|
-      item = Item.create!(
-        name:           record[:name],
-        description:    record[:description],
-        price_in_cents: ValueConverter.price_to_cents(record[:price]),
-        store_id:       params[:id]
-      )
+      item = Item.find_by(id: record[:id])
+      quantity = record.delete(:quantity)
+      if value = record.delete(:price)
+        record[:price_in_cents] = ValueConverter.price_to_cents(value)
+      end
+
+      if item
+        item = Item.update(record)
+      else
+        item = Item.create(record)
+      end
+      item.save
       Inventory.create!(
         store_id:   params[:id],
         item_id:    item.id,
-        quantity:   record[:quantity]
+        quantity:   quantity
       )
     end
     redirect_back
