bcb68e31b593b9917cd9a3117336e37a40f7bbdb
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-07-08 11:32:29 -0400

    watched item button fixed

--- app/controllers/watched_items_controller.rb
+++ app/controllers/watched_items_controller.rb
@@ -8,7 +8,7 @@ class WatchedItemsController < ApplicationController
   end
 
   def create
-    @watched_item = WatchedItem.new(item_id: params[:item_id], user_id: params[:user_id], price_in_cents: (params[:price_in_cents].to_i * 100))
+    @watched_item = WatchedItem.new(item_id: params["watched_item"][:item_id], user_id: params["watched_item"][:buyer_id], price_in_cents: (params["watched_item"][:price_in_cents].to_i * 100))
     if @watched_item.save
       flash[:notice] = "Item is being watched!"
       redirect_to :back 
